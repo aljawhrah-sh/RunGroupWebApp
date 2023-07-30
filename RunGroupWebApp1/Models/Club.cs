d1e8923e44a50ce92de8108feeb4288348efa5a3
Aljawhrah SH <aljawhrahsh@Aljawhrahs-MacBook-Pro.local>
2023-07-30 16:29:19 +0300

    test2

--- RunGroupWebApp1/Models/Club.cs
+++ RunGroupWebApp1/Models/Club.cs
@@ -23,7 +23,7 @@ namespace RunGroupWebApp1.Models
         public AppUser? AppUser { get; set; }
         public ICollection<Club> Clubs { get; set; }
         public ICollection<Race> Races { get; set; }
-        // tset
+        // tset 123
 
 
     }
